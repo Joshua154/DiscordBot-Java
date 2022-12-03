6c58aec89a7e474dc9973e45cc5b82fcdcc0808a
Joshua154 <joshineunert@gmail.com>
2022-12-03 05:03:20 +0100

    added Crit

---
+++ src/main/kotlin/de/joshua/dnd/CritCommand.kt
@@ -0,0 +1,75 @@
+package de.joshua.dnd
+
+import de.joshua.api.commands.slashcommands.SlashCommand
+import de.joshua.api.commands.slashcommands.SlashCommands
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.Command
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
+import net.dv8tion.jda.internal.interactions.CommandDataImpl
+import java.time.Instant
+
+@SlashCommands
+class CritCommand : SlashCommand {
+    override fun commandData(): CommandDataImpl {
+        return CommandDataImpl("crit", "crit")
+            .addOptions(
+                OptionData(OptionType.USER, "user", "User to roll for"),
+                OptionData(OptionType.BOOLEAN, "private", "Roles a crit privately"),
+                OptionData(OptionType.STRING, "location", "location").addChoices(
+                    Command.Choice("head", "head"),
+                    Command.Choice("arm", "arm"),
+                    Command.Choice("body", "body"),
+                    Command.Choice("leg", "leg")
+                )
+            )
+    }
+
+    override fun onExecute(event: SlashCommandInteractionEvent) {
+        val ephemeral = event.getOption("private") != null && event.getOption("private")?.asBoolean!!
+
+        var user = event.user
+        if(event.getOption("user") != null){
+            user = event.getOption("user")?.asUser!!
+        }
+
+        var crit: CritEntry? = Dice.getCrit()
+        if(event.getOption("location") != null){
+            crit = Dice.getCrit(getBodyPartNumber(event.getOption("location")?.asString!!))
+        }
+        if(crit ==  null){
+            event.reply("Error")
+            return
+        }
+
+
+        val embed = EmbedBuilder()
+            .setAuthor(
+                user.name,
+                null,
+                user.avatarUrl
+            )
+            .setTitle("${crit.description} (${crit.intensity})")
+            .addField("Location", "${Dice.getBodyPart(crit.location)} (${crit.location})", false)
+            .addField("Wounds", crit.wounds, false)
+            .addField("Additional Effects", crit.additionalEffects, false)
+            .setTimestamp(Instant.now())
+
+
+        event.replyEmbeds(embed.build()).setEphemeral(ephemeral).setActionRow(Dice.getActionRow()).queue()
+    }
+}
+
+/*
+
+`**Location**:
+${finalString} (_${roll}_)
+
+**Wounds**:
+${list[`${num}`].wounds}
+
+**Additional Effects**:
+${list[`${num}`].additionalEffects}`
+
+*/
--- src/main/kotlin/de/joshua/dnd/Dice.kt
+++ src/main/kotlin/de/joshua/dnd/Dice.kt
@@ -1,12 +1,25 @@
 package de.joshua.dnd
 
+import com.beust.klaxon.JsonReader
+import com.beust.klaxon.Klaxon
+import de.joshua.DiscordBot
+import de.joshua.dnd.buttons.BodyPartButton
+import de.joshua.dnd.buttons.CritButton
+import de.joshua.dnd.buttons.DiceButton
+import de.joshua.dnd.buttons.d10Button
+import net.dv8tion.jda.api.interactions.components.ItemComponent
 import java.awt.Color
+import java.io.StringReader
+import java.net.URL
+
 
 class Dice {
     companion object {
-        fun rollDice(max: Int): Int {
-            return (Math.random() * max).toInt()
+        fun rollDice(max: Int = 100): Int {
+            return (Math.random() * max + 1).toInt()
         }
+
+
 
         fun getColor(per: Double): Color {
             val percent = per / 100
@@ -25,6 +38,80 @@ class Dice {
             if (list.size < 2) return false
             return list.size == 3 || list[0] == list[1]
         }
+
+        fun getCrit(location: Int = this.rollDice(), intensity: Int = this.rollDice()): CritEntry {
+
+            var file: URL? = null
+
+            if(location <= 9){
+                file = DiscordBot::class.java.getResource("/crit/head.json")
+            }else if(location <= 24){
+                file = DiscordBot::class.java.getResource("/crit/arm.json")
+            }else if(location <= 44){
+                file = DiscordBot::class.java.getResource("/crit/arm.json")
+            }else if(location <= 79){
+                file = DiscordBot::class.java.getResource("/crit/body.json")
+            }else if(location <= 89){
+                file = DiscordBot::class.java.getResource("/crit/leg.json")
+            }else if(location <= 100){
+                file = DiscordBot::class.java.getResource("/crit/leg.json")
+            }
+
+            if(file == null) return CritEntry(0, "", "Error", "", "")
+
+
+            val klaxon = Klaxon()
+            val result = arrayListOf<TableEntry>()
+            JsonReader(StringReader(file.readText())).use { reader ->
+                reader.beginArray {
+                    while (reader.hasNext()) {
+                        /*val person = klaxon.parse<TableEntry>(reader)
+                        if (person != null) {
+                            result.add(person)
+                        }*/
+                        klaxon.parse<TableEntry>(reader)?.let { result.add(it) }
+                    }
+                }
+            }
+
+
+            var index = -1
+            for (i in result.size - 1 downTo 0) {
+                if (result.get(i).max <= intensity) {
+                    index = i
+                    break
+                }
+            }
+
+
+            return CritEntry(location, intensity.toString(), result[index].description, result[index].wounds, result[index].additionalEffects)
+        }
+
+        fun getBodyPart(number: Int): String {
+            if(number <= 9){
+                return "Head"
+            }else if(number <= 24){
+                return "Left Arm"
+            }else if(number <= 44){
+                return "Right Arm"
+            }else if(number <= 79){
+                return "Body"
+            }else if(number <= 89){
+                return "Left Leg"
+            }else if(number <= 100){
+                return "Right Leg"
+            }
+            return "Error"
+        }
+
+        fun getActionRow(): List<ItemComponent> {
+            val list = mutableListOf<ItemComponent>()
+            list.add(DiceButton().getButton())
+            list.add(d10Button().getButton())
+            list.add(BodyPartButton().getButton())
+            list.add(CritButton().getButton())
+            return list
+        }
     }
 }
 
@@ -37,8 +124,28 @@ fun Int.toDigits(base: Int = 10): List<Int> = sequence {
     }
 }.toList()
 
-class TableEntry(
+fun getBodyPartNumber(bodyPart: String): Int {
+    when(bodyPart){
+        "head" ->  return 0
+        "arm" ->  return 10
+        "body" ->  return 45
+        "leg" ->  return 80
+    }
+    return 0
+}
+
+
+data class TableEntry(
+    val max: Int,
     val description: String,
-    val wounds: Int,
-    val additionalEffects: String
+    val wounds: String,
+    val additionalEffects: String,
+)
+
+data class CritEntry(
+    val location: Int,
+    val intensity: String,
+    val description: String,
+    val wounds: String,
+    val additionalEffects: String,
 )
